09fc1d39533f548b0043f1005def12eb2af94e1d
Patrick Walls <walls.patrick.a@gmail.com>
2020-08-19 22:58:18 -0400

    added scroll effect and fixed styling

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-console.log('my files are connected');
-
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "#landing-section") {
+      let hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        500,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
